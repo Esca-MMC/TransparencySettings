244b40840f749815931ad3d2e7cf38ea6fcdcfa5
Esca-MMC <esca.mmc@gmail.com>
2021-06-09 15:49:07 -0500

    Add project files.

---
+++ TransparencySettings/ModConfig.cs
@@ -0,0 +1,43 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace TransparencySettings
+{
+    /// <summary>This mod's config.json setting data.</summary>
+    public class ModConfig
+    {
+        public ObjectSettings BuildingSettings = new ObjectSettings()
+        {
+            TileDistance = 3
+        };
+        public ObjectSettings BushSettings = new ObjectSettings();
+        public ObjectSettings TreeSettings = new ObjectSettings();
+        public KeybindSettings KeyBindings = new KeybindSettings();
+    }
+
+    /* Setting collections (used for descriptive organization in the config.json file) */
+
+    public class ObjectSettings
+    {
+        public bool Enable = true;
+        public bool BelowPlayerOnly = true;
+        public int TileDistance = 5;
+    }
+
+    public class KeybindSettings
+    {
+        private KeybindList _disableTransparency = new KeybindList(new Keybind(SButton.OemTilde, SButton.LeftShift));
+        public KeybindList DisableTransparency
+        {
+            get { return _disableTransparency; }
+            set { _disableTransparency = value ?? new KeybindList(); } //prevent null values
+        }
+
+        private KeybindList _fullTransparency = new KeybindList(new Keybind(SButton.OemTilde));
+        public KeybindList FullTransparency
+        {
+            get { return _fullTransparency; }
+            set { _fullTransparency = value ?? new KeybindList(); } //prevent null values
+        }
+    }
+}
